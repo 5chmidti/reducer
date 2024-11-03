271120701089eb9211d3d13f66ef3a6c4a25e32b
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-11-04 00:55:02 +0100

    fix initial ruff findings

--- reducer/reducer.py
+++ reducer/reducer.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 
-from argparse import ArgumentParser, Namespace, BooleanOptionalAction
 import json
-from os import chmod, stat
+import logging
 import re
+from argparse import ArgumentParser, BooleanOptionalAction, Namespace
+from multiprocessing import cpu_count
+from os import chmod, stat
+from pathlib import Path
 from shutil import copy, which
 from stat import S_IEXEC
 from subprocess import call, run
 from typing import Any
 from uuid import uuid4
-from rich.logging import RichHandler
-import logging
-from pathlib import Path
-from multiprocessing import cpu_count
 
+from rich.logging import RichHandler
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    level="INFO",
+    format=FORMAT,
+    datefmt="[%X]",
+    handlers=[RichHandler()],
 )
 
 log = logging.getLogger("rich")
@@ -25,7 +28,8 @@ log = logging.getLogger("rich")
 
 def init_argparse() -> ArgumentParser:
     parser = ArgumentParser(
-        description="Extract and run reductions for cvise/creduce from a project with compile_commands.json.",
+        description="Extract and run reductions for cvise/creduce from a"
+        " project with compile_commands.json.",
     )
     parser.add_argument(
         "source_file",
@@ -66,7 +70,8 @@ def init_argparse() -> ArgumentParser:
     )
     parser.add_argument(
         "--verifying-compiler-args",
-        help="Arguments to call the verifying compiler with instead of the arguments from the crashing compiler.",
+        help="Arguments to call the verifying compiler with instead of the"
+        " arguments from the crashing compiler.",
         required=False,
         type=str,
     )
@@ -92,14 +97,15 @@ def init_argparse() -> ArgumentParser:
     )
     parser.add_argument(
         "--timeout",
-        help="Timeout for the interestingness command, when timing out is considered the issue (time-out -> interesting).",
+        help="Timeout for the interestingness command, when timing out is"
+        " considered the issue (time-out -> interesting).",
         required=False,
         type=int,
     )
     return parser
 
 
-def set_reduce_bin(args: Namespace):
+def set_reduce_bin(args: Namespace) -> None:
     if args.reduce_bin:
         return
 
@@ -114,7 +120,7 @@ def set_reduce_bin(args: Namespace):
     raise RuntimeError("Could not find reduction binaries cvise or creduce")
 
 
-def replace_path(string: str, file: Path, new_path_str: str):
+def replace_path(string: str, file: Path, new_path_str: str) -> str:
     return (
         string.replace(str(file), new_path_str)
         .replace(str(file.absolute()), new_path_str)
@@ -122,7 +128,11 @@ def replace_path(string: str, file: Path, new_path_str: str):
     )
 
 
-def get_compile_commands_entry_for_file(source_file: Path, build_dir: Path, cwd: Path):
+def get_compile_commands_entry_for_file(
+    source_file: Path,
+    build_dir: Path,
+    cwd: Path,
+) -> list[Any]:
     compile_commands_file = build_dir / "compile_commands.json"
     new_file_path = cwd.absolute() / source_file.name
     log.info(str(new_file_path))
@@ -131,80 +141,95 @@ def get_compile_commands_entry_for_file(source_file: Path, build_dir: Path, cwd:
     raw_commands = re.sub(r"-o [^ ]*\.o", "-o output.cpp.o", raw_commands)
     raw_commands = raw_commands.replace("-c ", f"-I{source_file.parent} -c ")
     raw_commands = raw_commands.replace(
-        "-c ", "-Wfatal-errors -Wno-invalid-constexpr -c "
+        "-c ",
+        "-Wfatal-errors -Wno-invalid-constexpr -c ",
     )
     raw_commands = raw_commands.replace("-Werror", "")
 
     commands = json.loads(raw_commands)
-    res = [x for x in commands if source_file.name in x["file"]]
-    return res
+    return [x for x in commands if source_file.name in x["file"]]
 
 
 def remove_explicit_path(compile_command: str, cwd: Path) -> str:
     return compile_command.replace(str(cwd) + "/", "")
 
 
-def write_compile_commands(compile_commands: Any, cwd: Path):
+def write_compile_commands(compile_commands: Any, cwd: Path) -> None:
     new_compile_commands_path = cwd.absolute() / "compile_commands.json"
-    with open(str(new_compile_commands_path), "w") as file:
-        file.write(json.dumps(compile_commands))
+    new_compile_commands_path.write_text(json.dumps(compile_commands))
 
 
-def create_interestingness_test(args: Namespace, cwd: Path, compile_command: str):
+def create_interestingness_test(
+    args: Namespace,
+    cwd: Path,
+    compile_command: str,
+) -> None:
     compile_command = remove_explicit_path(compile_command, cwd)
 
-    with open(f"{cwd}/test.sh", "w") as file:
-        file.write("#!/bin/bash\n")
+    file = Path(f"{cwd}/test.sh")
+    file.write_text("#!/bin/bash\n")
 
-        if args.compile_error:
-            if args.verifying_compiler:
-                verifying_compiler_args: str
-                if args.verifying_compiler_args:
-                    verifying_compiler_args = args.verifying_compiler_args
-                else:
-                    verifying_compiler_args = (
-                        compile_command[compile_command.find(" ") :]
-                        .replace("-fcolor-diagnostics", "")
-                        .replace("-Wdocumentation", "")
-                        .replace("-fopenmp=libomp", "-fopenmp")
-                    )
-                file.write(args.verifying_compiler + f" {verifying_compiler_args} && ")
-            if args.timeout:
-                file.write(
-                    "! " + compile_command + " -fno-color-diagnostics > log.txt 2>&1"
-                )
+    if args.compile_error:
+        if args.verifying_compiler:
+            verifying_compiler_args: str
+            if args.verifying_compiler_args:
+                verifying_compiler_args = args.verifying_compiler_args
             else:
-                file.write(
-                    f"! timeout {args.timeout} "
-                    + compile_command
-                    + " -fno-color-diagnostics > log.txt 2>&1"
+                verifying_compiler_args = (
+                    compile_command[compile_command.find(" ") :]
+                    .replace("-fcolor-diagnostics", "")
+                    .replace("-Wdocumentation", "")
+                    .replace("-fopenmp=libomp", "-fopenmp")
                 )
+            file.write_text(
+                args.verifying_compiler + f" {verifying_compiler_args} && ",
+            )
+        if args.timeout:
+            file.write_text(
+                "! "
+                + compile_command
+                + " -fno-color-diagnostics > log.txt 2>&1",
+            )
         else:
-            file.write(
-                compile_command
-                + " -Wfatal-errors -fno-color-diagnostics > log.txt 2>&1"
+            file.write_text(
+                f"! timeout {args.timeout} "
+                + compile_command
+                + " -fno-color-diagnostics > log.txt 2>&1",
             )
+    else:
+        file.write_text(
+            compile_command
+            + " -Wfatal-errors -fno-color-diagnostics > log.txt 2>&1",
+        )
 
-        if args.interesting_command:
-            interesting_command = replace_path(
-                args.interesting_command, args.source_file, args.source_file.name
+    if args.interesting_command:
+        interesting_command = replace_path(
+            args.interesting_command,
+            args.source_file,
+            args.source_file.name,
+        )
+        interesting_command = re.sub(
+            r"-p[ =]?[^ ]*",
+            f"-p {cwd!s}",
+            interesting_command,
+        )
+        log.info(f"interesting_command: {interesting_command}")
+
+        if args.timeout:
+            file.write_text(
+                f" && ! timeout {args.timeout} {interesting_command}\n",
             )
-            interesting_command = re.sub(
-                r"-p[ =]?[^ ]*", f"-p {str(cwd)}", interesting_command
-            )
-            log.info(f"interesting_command: {interesting_command}")
+        else:
+            file.write_text(f" && {interesting_command}\n")
 
-            if args.timeout:
-                file.write(f" && ! timeout {args.timeout} {interesting_command}\n")
-            else:
-                file.write(f" && {interesting_command}\n")
-
-        chmod(file.name, stat(file.name).st_mode | S_IEXEC)
+    file.chmod(file.stat().st_mode | S_IEXEC)
 
 
-def setup_test_folder(args: Namespace, cwd: Path):
+def setup_test_folder(args: Namespace, cwd: Path) -> None:
     compile_commands = get_compile_commands_entry_for_file(
-        args.source_file, args.build_dir, cwd
+        args.source_file,
+        args.build_dir,
+        cwd,
     )
     write_compile_commands(compile_commands, cwd)
     file_path: Path = args.source_file
@@ -216,9 +241,10 @@ def setup_test_folder(args: Namespace, cwd: Path):
     create_interestingness_test(args, cwd, compile_command)
 
 
-def preprocess_file(cwd: Path, file_path: Path, compile_command: str):
+def preprocess_file(cwd: Path, file_path: Path, compile_command: str) -> None:
     c = compile_command.replace(
-        "-o output.cpp.o", f"-E -P -o {cwd / file_path.name}.tmp"
+        "-o output.cpp.o",
+        f"-E -P -o {cwd / file_path.name}.tmp",
     ).split(" ")
 
     log.info("preprocess: " + " ".join(c))
@@ -227,13 +253,13 @@ def preprocess_file(cwd: Path, file_path: Path, compile_command: str):
     copy(f"{cwd / file_path.name}.tmp", f"{cwd / file_path.name}")
 
 
-def load_compile_commands(dir: Path):
-    compile_commands_file = dir / "compile_commands.json"
+def load_compile_commands(build_dir: Path) -> Any:
+    compile_commands_file = build_dir / "compile_commands.json"
     compile_commands_raw = compile_commands_file.read_text()
     return json.loads(compile_commands_raw)
 
 
-def get_cpp_std_from_compile_commands(cwd: Path):
+def get_cpp_std_from_compile_commands(cwd: Path) -> str:
     cpp_std = "c++20"
     compile_commands = load_compile_commands(cwd)
     compile_command: str = compile_commands[0]["command"]
@@ -241,19 +267,21 @@ def get_cpp_std_from_compile_commands(cwd: Path):
     if cpp_std_pos != -1:
         is_gnu = compile_command[cpp_std_pos + 5] == "g"
         if is_gnu:
-            cpp_std = f"c++{compile_command[cpp_std_pos + 10 : cpp_std_pos + 12]}"
+            cpp_std = (
+                f"c++{compile_command[cpp_std_pos + 10 : cpp_std_pos + 12]}"
+            )
         else:
             cpp_std = f"{compile_command[cpp_std_pos + 6 : cpp_std_pos + 11]}"
     return cpp_std
 
 
 def get_csvise_supported_cpp_std(args: Namespace, cpp_std: str) -> str:
-    res = run([args.reduce_bin, "--help"], capture_output=True)
+    res = run([args.reduce_bin, "--help"], capture_output=True, check=False)
     help_msg = res.stdout.decode()
 
     clang_delta_std_flag = "--clang-delta-std {"
     clang_delta_std_loc = help_msg.find(clang_delta_std_flag) + len(
-        clang_delta_std_flag
+        clang_delta_std_flag,
     )
 
     help_msg = help_msg[
@@ -270,12 +298,12 @@ def get_csvise_supported_cpp_std(args: Namespace, cpp_std: str) -> str:
     return "c++20"
 
 
-def reduce_input(args: Namespace, cwd: Path):
+def reduce_input(args: Namespace, cwd: Path) -> None:
     invocation: list[str] = [args.reduce_bin]
 
     if "cvise" in args.reduce_bin:
         invocation.append(
-            f"--clang-delta-std={get_csvise_supported_cpp_std(args, get_cpp_std_from_compile_commands(cwd))}"
+            f"--clang-delta-std={get_csvise_supported_cpp_std(args, get_cpp_std_from_compile_commands(cwd))}",
         )
         invocation.append("--to-utf8")
 
@@ -297,7 +325,7 @@ def reduce_input(args: Namespace, cwd: Path):
         raise RuntimeError("reduction invokation failed")
 
 
-def reduce_existing(args: Namespace):
+def reduce_existing(args: Namespace) -> None:
     set_reduce_bin(args)
 
     existing_path = args.rerun_existing
@@ -315,7 +343,7 @@ def reduce_existing(args: Namespace):
     reduce_input(args, existing_path)
 
 
-def reduce_new(args: Namespace):
+def reduce_new(args: Namespace) -> None:
     set_reduce_bin(args)
 
     args.source_file = args.source_file.resolve()
@@ -326,14 +354,16 @@ def reduce_new(args: Namespace):
 
     if args.interesting_command:
         args.interesting_command = replace_path(
-            args.interesting_command, args.build_dir, str(cwd)
+            args.interesting_command,
+            args.build_dir,
+            str(cwd),
         )
 
     setup_test_folder(args, cwd)
     reduce_input(args, cwd)
 
 
-def main():
+def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
 
@@ -342,7 +372,9 @@ def main():
         return
 
     if args.verifying_compiler_args and not args.verifying_compiler:
-        log.error("option --verifying-compiler-args requires --verifying-compiler")
+        log.error(
+            "option --verifying-compiler-args requires --verifying-compiler",
+        )
         return
 
     log.info(f"{args}")
