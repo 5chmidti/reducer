48efe2db3319bced1e5a9e2fe998631183490fc7
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-12-21 23:01:34 +0100

    init compiler_crash driver

---
+++ reducer/driver/compiler_crash.py
@@ -0,0 +1,82 @@
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from stat import S_IEXEC, S_IROTH, S_IXOTH
+
+from reducer.lib.driver import Driver
+from reducer.lib.grep import grep_file_content
+from reducer.lib.setup import (
+    get_compile_command,
+    remove_explicit_path,
+)
+
+
+class CompilerCrashDriver(Driver):
+    def __init__(self) -> None:
+        pass
+
+    def add_arguments(self, common_parser: ArgumentParser, sub_parser) -> None:
+        parser = sub_parser.add_parser(
+            name="compiler-crash",
+            help="Reduce a compiler crash",
+            parents=[common_parser],
+        )
+        parser.add_argument(
+            "--verifying-compiler",
+            help="The compiler that checks if the reduced code is still valid.",
+            required=False,
+            type=str,
+        )
+        parser.add_argument(
+            "--verifying-compiler-args",
+            help="Arguments to call the verifying compiler with instead of the"
+            " arguments from the crashing compiler.",
+            required=False,
+            type=str,
+        )
+
+    def setup(self, args: Namespace, cwd: Path) -> None:
+        super().setup(args, cwd)
+
+    def create_interestingness_test(
+        self,
+        args: Namespace,
+        cwd: Path,
+        compile_command_json: dict[str, str],
+    ) -> None:
+        compile_command = get_compile_command(
+            remove_explicit_path(compile_command_json["command"], cwd),
+            cwd,
+        )
+
+        file = Path(f"{cwd}/test.sh")
+        file_content: str = "#!/bin/sh\n"
+
+        if args.timeout:
+            file_content = file_content + f"! timeout {args.timeout} "
+
+        verifying_compiler_args: str
+        if args.verifying_compiler_args:
+            verifying_compiler_args = args.verifying_compiler_args
+        else:
+            verifying_compiler_args = compile_command[compile_command.find(" ") :]
+        file_content = str(
+            file_content
+            + str(args.verifying_compiler)
+            + f" {verifying_compiler_args} && ",
+        )
+
+        if args.timeout:
+            file_content = file_content + f"! timeout {args.timeout} "
+        file_content = str(
+            file_content
+            + get_compile_command(compile_command, cwd)
+            + " -Wfatal-errors -fno-color-diagnostics > log.txt 2>&1",
+        )
+
+        if args.grep:
+            file_content = file_content + grep_file_content(args.grep, "log.txt")
+        if args.grep_file:
+            file_content = file_content + grep_file_content(args.grep_file, "log.txt")
+
+        file.write_text(file_content)
+        file.chmod(file.stat().st_mode | S_IEXEC | S_IXOTH | S_IROTH)
--- reducer/reducer.py
+++ reducer/reducer.py
@@ -7,6 +7,7 @@ from shutil import which
 from uuid import uuid4
 
 from reducer.driver.clang_tidy import ClangTidyDriver
+from reducer.driver.compiler_crash import CompilerCrashDriver
 from reducer.lib.driver import Driver
 from reducer.lib.log import log
 from reducer.lib.setup import (
@@ -43,27 +44,6 @@ def init_argparse() -> ArgumentParser:
     parser.add_argument(
         "--interesting-command",
         help="Command satisfying interestingness test properties.",
-        required=False,
-        type=str,
-    )
-    parser.add_argument(
-        "--compile-error",
-        help="Reduce an internal compiler error.",
-        default=False,
-        required=False,
-        type=bool,
-        action=BooleanOptionalAction,
-    )
-    parser.add_argument(
-        "--verifying-compiler",
-        help="The compiler that checks if the reduced code is still valid.",
-        required=False,
-        type=str,
-    )
-    parser.add_argument(
-        "--verifying-compiler-args",
-        help="Arguments to call the verifying compiler with instead of the"
-        " arguments from the crashing compiler.",
         required=False,
         type=str,
     )
@@ -142,17 +122,8 @@ def main() -> None:
         dest="sub",
     )
     ClangTidyDriver().add_arguments(common_parser, sub_parser)
+    CompilerCrashDriver().add_arguments(common_parser, sub_parser)
     args = parser.parse_args()
-
-    if args.compile_error and not args.verifying_compiler:
-        log.error("option --compile-error requires --verifying-compiler")
-        return
-
-    if args.verifying_compiler_args and not args.verifying_compiler:
-        log.error(
-            "option --verifying-compiler-args requires --verifying-compiler",
-        )
-        return
 
     log.info(f"{args}")
 
@@ -190,6 +161,8 @@ def main() -> None:
     match args.sub:
         case "tidy":
             driver = ClangTidyDriver()
+        case "compiler-crash":
+            driver = CompilerCrashDriver()
 
     if driver is None:
         return
