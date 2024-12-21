64ec147f865d1562b052e2eaa105d1385151b37e
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-12-21 22:11:02 +0100

    fix missing ] in prompt

--- reducer/lib/prompt.py
+++ reducer/lib/prompt.py
@@ -2,7 +2,7 @@ from reducer.lib.log import log
 
 
 def prompt_yes_no(question: str, assume_yes: bool = True) -> bool:
-    log.info(f"{question} [{"Y" if assume_yes else "y" }/{"n" if assume_yes else "N"}")
+    log.info(f"{question} [{"Y" if assume_yes else "y" }/{"n" if assume_yes else "N"}]")
     decision = input().lower()
     match decision:
         case "y":
